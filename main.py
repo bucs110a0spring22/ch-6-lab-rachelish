cf8bda81f7e8b7e88882ce3fe664fdb3db4fb03d
Rachel E E <62678193+rachelish@users.noreply.github.com>
2022-03-28 02:25:02 +0000

    i finished part A

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 
+
 def seq3np1(n):
+    count = 0
     """
         Print the 3n+1 sequence from n, terminating when it reaches 1.
         args: n (int) starting value for 3n+1 sequence
         return: None
     """
     while(n != 1):
-        print(n)
+        count += 1
+        #print(n)
         if(n % 2) == 0:        # n is even
             n = n // 2
         else:                 # n is odd
             n = n * 3 + 1
-    print(n)                  # the last print is 1
-
+   # print(n)                  # the last print is 1
+    return count
+def graph(turtle, upper_bound2, ):
+  turtle = turtle.Turtle()
+  wn = turtle.Screen 
+  upper_bound2 = int(input("Input upper bound: "))
+  screen = turtle.setworldcoordinates(-10, -10, 10, 10)
+  max_iteration = upper_bound2
+  
+  
 def main():
-	seq3np1(3)
+  upper_bound = int(input("Please input an upper bound number, bitch. Must be positive: "))
+  if(upper_bound < 0):
+    return
+    #or use exit()
+  for start in range(1, upper_bound+1):
+    seq3np1(upper_bound)
+    print("Current loop value: ",start,"Number of iterations: ", seq3np1(start))
+  seq3np1(3)
 main()
