26b965695b3b8023e7f7e603ef403d046db009ac
Rachel E E <62678193+rachelish@users.noreply.github.com>
2022-03-28 15:34:20 +0000

    i am down to the last of part B

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import turtle
 
 def seq3np1(n):
     count = 0
@@ -16,21 +16,36 @@ def seq3np1(n):
             n = n * 3 + 1
    # print(n)                  # the last print is 1
     return count
-def graph(turtle, upper_bound2, ):
-  turtle = turtle.Turtle()
-  wn = turtle.Screen 
-  upper_bound2 = int(input("Input upper bound: "))
-  screen = turtle.setworldcoordinates(-10, -10, 10, 10)
-  max_iteration = upper_bound2
+    #return seq3np1(n) ???
+def graph(upper_bound):
+  frank = turtle.Turtle()
+  turtle_two = turtle.Turtle()
+  wn = turtle.Screen() 
+  screen = turtle.setworldcoordinates(0, 0, 10, 10)
+  max_so_far = 0
+  string = ""
+  for i in range(1, upper_bound+1):
+    result = seq3np1(upper_bound)
+    if result > max_so_far:
+      string = "Maximum so far: ", i, result
+      max_so_far=result
+      turtle.write(string)
+      max_iteration = upper_bound
+      screen = turtle.setworldcoordinates(0, 0, i+10, max_so_far+10)
+      turtle.pd()
+      turtle.goto(max_so_far)
+  turtle.exitonclick()
   
+
   
 def main():
-  upper_bound = int(input("Please input an upper bound number, bitch. Must be positive: "))
+  upper_bound = int(input("Please input an upper bound number. Must be positive: "))
   if(upper_bound < 0):
     return
     #or use exit()
   for start in range(1, upper_bound+1):
     seq3np1(upper_bound)
     print("Current loop value: ",start,"Number of iterations: ", seq3np1(start))
+  graph(upper_bound)
   seq3np1(3)
 main()
